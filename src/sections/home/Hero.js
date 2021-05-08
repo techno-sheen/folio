9ba8a50f62d02fb06fcc6fd2c40568da11cbff2e
technosheen <sean@technosheen.com>
2021-05-08 02:48:29 -0400

    initial

---
+++ src/sections/home/Hero.js
@@ -0,0 +1,58 @@
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { Container, Row, Col } from "react-bootstrap";
+
+import GlobalContext from "../../context/GlobalContext";
+import { Title, Section, Box, Text, ButtonIcon } from "../../components/Core";
+
+import bgHeroPattern from "../../assets/image/webp/hero-pattern.webp";
+
+const SectionStyled = styled(Section)`
+  &::before {
+    position: absolute;
+    top: 0;
+    content: "";
+    width: 100%;
+    height: 120%;
+    background: url(${bgHeroPattern}) top center no-repeat;
+    background-size: cover;
+    z-index: -1;
+  }
+`;
+
+const Hero = () => {
+  const gContext = useContext(GlobalContext);
+
+  return (
+    <>
+      {/* <!-- Hero Area --> */}
+      <SectionStyled hero className="position-relative">
+        <Container>
+          <Row className="align-items-center">
+            <Col lg="7">
+              <Box mt={5}>
+                <Title>Give your site some <br />
+                  <em>SHEEN</em> </Title>
+                <Text pb="1.875rem" pt="1.25rem" color="heading">
+                  Miami based software developer.
+                </Text>
+                <Box>
+                  <ButtonIcon
+                    onClick={(e) => {
+                      e.preventDefault();
+                      gContext.toggleContact();
+                    }}
+                  >
+                    Lets talk
+                  </ButtonIcon>
+                </Box>
+              </Box>
+            </Col>
+          </Row>
+        </Container>
+      </SectionStyled>
+    </>
+  );
+};
+
+export default Hero;
